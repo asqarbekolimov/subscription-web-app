0f53d39856d9d73d0c16b6b969207e1992f95b85
asqarbekolimov <mr.coderx.pro@gmail.com>
2023-11-30 11:06:22 +0500

    feat: header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
+import { Header, Row } from "@/components";
 import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex min-h-screen flex-col items-center py-2">
       <Head>
         <title>Movie App</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href="favicon.ico" />
+        <link rel="shortcut icon" href="logo.svg" />
       </Head>
-      <div>hello world</div>;
-    </>
+      <Header />
+      <main>
+        {/* hero */}
+        <section>
+          {/* row */}
+          {/* bigRow */}
+        </section>
+      </main>
+    </div>
   );
 }
