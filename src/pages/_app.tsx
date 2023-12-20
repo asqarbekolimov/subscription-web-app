7bb62798959687b223d3a032d4706ea0bd31fba0
asqarbek olimov <88034288+asqarbekolimov@users.noreply.github.com>
2023-12-20 15:03:46 +0500

    feat: Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import AuthContextProvider from "@/context/auth.context";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
       <Component {...pageProps} />
+      <Analytics />
     </AuthContextProvider>
   );
 }
